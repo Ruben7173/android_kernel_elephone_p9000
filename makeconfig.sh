2bb5a8a8637e8a4371f13e8faa7923aacc68e496
Ruben Craveiro <rubencr7173@gmail.com>
2017-08-09 21:13:09 +0200

    Based on Tribual repos

---
+++ makeconfig.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+DEFCONFIG=lineage_p9000_defconfig
+make clean && make mrproper
+export CROSS_COMPILE=~/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export ARCH=arm64
+make -j8 $DEFCONFIG
---
+++ makekernel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DEFCONFIG=aokp_p9000_defconfig
+KERNEL=aokp
+if [  -f $KERNEL ]; then
+	rm $KERNEL
+fi
+if [  -f $DEFCONFIG ]; then
+	rm $DEFCONFIG
+fi
+make clean && make mrproper
+export CROSS_COMPILE=~/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export ARCH=arm64
+make -j8 $DEFCONFIG
+make -j8
+cp arch/arm64/boot/Image.gz-dtb $KERNEL
